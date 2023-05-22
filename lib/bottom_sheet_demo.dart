de247cdafb1cb0d590d99e9d28e9a751f88ee944
veerbhayani <virbhayani137@gmail.com>
2023-05-22 17:06:56 +0530

    first_commit

---
+++ lib/bottom_sheet_demo.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+
+class BottomSheetDemo extends StatelessWidget {
+  const BottomSheetDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) => Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                  ),
+                ),
+                child: const Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text('Create'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          child: const Text('Show Bottom Sheet'),
+        ),
+      ),
+    );
+  }
+}
