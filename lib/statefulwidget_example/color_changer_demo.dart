6c1abb52ad82cf41594bee6af5fbf0aa9c6c40e1
veerbhayani <virbhayani137@gmail.com>
2023-06-12 16:27:26 +0530

    first_commit

---
+++ lib/statefulwidget_example/color_changer_demo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ColorChangerDemo extends StatefulWidget {
+  const ColorChangerDemo({super.key});
+  @override
+  State<ColorChangerDemo> createState() => _ColorChangerDemoState();
+}
+
+class _ColorChangerDemoState extends State<ColorChangerDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
