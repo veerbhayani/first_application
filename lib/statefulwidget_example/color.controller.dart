e0d1170f1ec063ea54e0c96a0349d7cf1da62e99
veerbhayani <virbhayani137@gmail.com>
2023-06-13 12:37:38 +0530

    first_commit

--- lib/statefulwidget_example/color.controller.dart
+++ lib/statefulwidget_example/color.controller.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+List<Color> containerColors = [
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.yellow,
+  Colors.orange,
+  Colors.purple,
+  Colors.pink,
+  Colors.teal,
+  Colors.brown,
+];
--- lib/statefulwidget_example/color_changer_demo.dart
+++ lib/statefulwidget_example/color_changer_demo.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'color.controller.dart';
+
 class ColorChangerDemo extends StatefulWidget {
   const ColorChangerDemo({super.key});
   @override
@@ -9,6 +11,25 @@ class ColorChangerDemo extends StatefulWidget {
 class _ColorChangerDemoState extends State<ColorChangerDemo> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: containerColors.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {},
+            child: Container(
+              color: containerColors[index],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
