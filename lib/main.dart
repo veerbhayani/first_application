244d0caee0393d892525507cf52e234066a64afa
veerbhayani <virbhayani137@gmail.com>
2023-06-22 01:06:05 +0530

    second_commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        useMaterial3: false,
+        useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
       home: const QuizPage(),
