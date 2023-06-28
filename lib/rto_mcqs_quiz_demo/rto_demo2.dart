ec98fe8a306dbe34eac047aa5e7a5c29a533e0aa
veerbhayani <virbhayani137@gmail.com>
2023-06-28 15:53:24 +0530

    files location changes

--- lib/rto_mcqs_quiz_demo/rto_demo2.dart
+++ lib/rto_mcqs_quiz_demo/rto_demo2.dart
@@ -1,4 +1,4 @@
-import 'package:first_application/row_data.dart';
+import 'package:first_application/raw_data/rto.raw.data.dart';
 import 'package:flutter/material.dart';
 
 import 'mcq_model.dart';
