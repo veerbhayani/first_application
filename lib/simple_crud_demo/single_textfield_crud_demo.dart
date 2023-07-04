3406a37643097f28665a325756e13348c1f4ca3f
veerbhayani <virbhayani137@gmail.com>
2023-07-04 15:20:35 +0530

    single textfield crud demo

--- lib/simple_crud_demo/single_textfield_crud_demo.dart
+++ lib/simple_crud_demo/single_textfield_crud_demo.dart
@@ -16,14 +16,25 @@ class _SingleTextFieldCrudDemoState extends State<SingleTextFieldCrudDemo> {
     super.dispose();
   }
 
+  List<dynamic> userData = [];
+  bool isUpdate = false;
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          TextField(
-            controller: _textUserNameController,
+          Padding(
+            padding: const EdgeInsets.all(
+              10,
+            ),
+            child: TextField(
+              controller: _textUserNameController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
           ),
           Padding(
             padding: const EdgeInsets.all(
@@ -31,22 +42,57 @@ class _SingleTextFieldCrudDemoState extends State<SingleTextFieldCrudDemo> {
             ),
             child: MaterialButton(
               color: Colors.blue,
-              onPressed: () {},
-              child: const Text(
-                'Submit',
-                style: TextStyle(
+              onPressed: isUpdate
+                  ? () {
+                      userData[selectedIndex] = _textUserNameController.text;
+                      isUpdate = false;
+                      _textUserNameController.clear();
+                      setState(() {});
+                    }
+                  : () {
+                      userData.add(_textUserNameController.text);
+                      _textUserNameController.clear();
+                      setState(() {});
+                    },
+              child: Text(
+                isUpdate ? 'Update' : 'Submit',
+                style: const TextStyle(
                   color: Colors.white,
                 ),
               ),
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return const SizedBox();
-              },
-            ),
-          ),
+          userData.isEmpty
+              ? const Text(
+                  'There is no data',
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                )
+              : Expanded(
+                  child: ListView.builder(
+                    itemCount: userData.length,
+                    itemBuilder: (context, index) {
+                      return Dismissible(
+                        onDismissed: (direction) {
+                          userData.removeAt(index);
+                          setState(() {});
+                        },
+                        key: UniqueKey(),
+                        child: ListTile(
+                          onTap: () {
+                            isUpdate = true;
+                            selectedIndex = index;
+                            setState(() {});
+                          },
+                          title: Text(
+                            userData[index],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
         ],
       ),
     );
